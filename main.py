ec6074cb98f622fafcf98bc8548156617590f4e8
akash9795 <107749192+akash9795@users.noreply.github.com>
2023-07-31 02:48:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import textbase
+from textbase.message import Message
+from textbase import models
+
+from typing import List
+
+# Load your OpenAI API key
+models.OpenAI.api_key = "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"
+# or from environment variable:
+# models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+
+# Prompt for GPT-3.5 Turbo
+SYSTEM_PROMPT = "I want you to act as a mental health adviser. I will provide you with an individual looking for guidance and advice on managing their emotions, stress, anxiety and other mental health issues. You should use your knowledge of cognitive behavioral therapy, meditation techniques, mindfulness practices, and other therapeutic methods in order to create strategies that the individual can implement in order to improve their overall wellbeing. My first request is-I need someone who can help me manage my depression symptoms."
+
+
+@textbase.chatbot("talking-bot")
+def on_message(message_history: List[Message], state: dict = None):
+    """Your chatbot logic here
+    message_history: List of user messages
+    state: A dictionary to store any stateful information
+
+    Return a string with the bot_response or a tuple of (bot_response: str, new_state: dict)
+    """
+
+    if state is None or "counter" not in state:
+        state = {"counter": 0}
+    else:
+        state["counter"] += 1
+
+    # # Generate GPT-3.5 Turbo response
+    bot_response = models.OpenAI.generate(
+        system_prompt=SYSTEM_PROMPT,
+        message_history=message_history,
+        model="gpt-3.5-turbo",
+    )
+
+    return bot_response, state
